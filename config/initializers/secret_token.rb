4e8096db936e59201cfd9458521a5775d6ed563e
Jack Andrew <jacktandrew@gmail.com>
2011-06-20 05:33:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ferdinand::Application.config.secret_token = '450e92eff59e6b6989ffe1a3ded6205246f498cf9eb10239e4df6096e54f5d8b2cc7406bde36be7f46218478fd1c4aac858fe9c7e4ad582438cb3adf27265a75'
