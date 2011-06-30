79159114b732323d1bfb1e4065ca77a5cce3eb08
Jack Andrew <jacktandrew@gmail.com>
2011-06-30 14:46:10 -0700

    jqtouch add

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
   end
-
+  
+  def show
+    @recipe = Recipe.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.mobile { render :layout => false }
+    end
+  end
 end
